c864c1bcfc945d4bba698401ef17cc85d0d3f4e7
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-30 23:52:38 -0500

    installed gems, set up application controller, updated config file

--- ruby/config.ru
+++ ruby/config.ru
@@ -3,14 +3,12 @@ require 'sinatra/activerecord'
 
 # models
 require './models/UserModel'
-# require './models/StudentModel'
 require './models/SongModel'
 require './models/PracticeLogModel'
 
 # controllers
 require './controllers/ApplicationController'
 require './controllers/UserController'
-# require './controllers/StudentController'
 require './controllers/SongController'
 require './controllers/PracticeLogController'
 
@@ -21,9 +19,6 @@ map('/') {
 map('/users') {
 	run UserController
 }
-# map('/students') {
-# 	run StudentController
-# }
 map('/songs') {
 	run SongController
 }
---
+++ ruby/controllers/ApplicationController.rb
@@ -0,0 +1,43 @@
+class ApplicationController < Sinatra::Base
+
+	require 'bundler'
+	Bundler.require()
+
+	# require './config/environments'
+
+	ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql',
+		:database => 'employer'
+	)
+	use Rack::Session::Cookie,	:key => 'rack.session',
+		:path => '/',
+		:secret => 'your_secret'
+
+	get '/' do
+		{
+			success: false,
+			message: "Please consult the API documentation at: https://github.com/irwinx16/project3_API"
+		}.to_json
+	end
+
+	not_found do
+		halt 404
+	end
+
+	register Sinatra::CrossOrigin
+
+	configure do
+		enable :cross_origin
+	end
+
+	set :allow_origin, :any # you can specifiy origins here, we'll just say any
+	set :allow_methods, [:get, :post, :options, :put, :patch, :delete]
+	set :allow_credentials, true # session info / API key enable
+
+	options '*' do
+		response.headers['Allow'] = 'HEAD, GET, POST, PUT, PATCH, DELETE'
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+	end
+
+end
