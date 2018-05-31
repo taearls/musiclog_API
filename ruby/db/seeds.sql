5e821f261ea3071ac9b70335ffe54385a628e4b6
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-31 16:55:18 -0500

    added login info for seeds in comments for easy access

--- ruby/db/seeds.sql
+++ ruby/db/seeds.sql
@@ -1,8 +1,11 @@
 -- users data
+-- email is 'jamesreeves13@aol.com'
 -- password is 'james'
 INSERT INTO users (email, password_digest, name, school, grade, student_phone, text_student, parent_phone, text_parent, lesson_location, lesson_day, lesson_time, additional_info) VALUES ('jamesreeves13@aol.com', '$2a$10$bZDm28Ho6U9kQ1xqZ8N/NuMlGOHPUKK3ApDYBYUSff/wykPJuJ1WK', 'Fiona Reeves', 'Wild Rose Elementary', '3rd Grade', '123-345-6789', true, '(773)677-1866 Maureen, (773)425-8954 Jim', true, '36W097 Indian mound road, stc, 60174', 'Tuesday', '4:00 PM', 'She loves her violin!');
+-- email is 'erinslater73@gmail.com'
 -- password is erin
 INSERT INTO users (email, password_digest, name, school, grade, student_phone, text_student, parent_phone, text_parent, lesson_location, lesson_day, lesson_time, additional_info) VALUES ('erinslater73@gmail.com', '$2a$10$iuVnuMMOtif5Ax2qIDCfNuNQYqikA6Je.aQCivSi3AnI27gh5o0Fm', 'Lauren Slater', 'Fox Ridge', 'Grade 5', '555-555-0123', false, '773.771.0135', true, '2607 Turnberry Rd, 60174', 'Wednesday', '4:00 PM', 'He struggles paying attention.');
+-- email is 'chen.anna@gmail.com'
 -- password is 'tyler'
 INSERT INTO users (email, password_digest, name, school, grade, student_phone, text_student, parent_phone, text_parent, lesson_location, lesson_day, lesson_time, additional_info) VALUES ('chen.anna@gmail.com', '$2a$10$2IRtJP2EN.4zV85EZcatce8he.DfcdK9EeDTiN6bI6KzQk4waW.WS', 'Tyler', 'Norton Creek Elementary', '3rd Grade', '111-222-333', false, '6303383395', true, '1940 Cool Creek Dr. St. Charles IL 60174', 'Wednesday', '5:30 PM', 'Very motivated to learn.');
 
